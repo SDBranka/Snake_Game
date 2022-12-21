eeae524f7af1328fedca98b700efe420e132d60e
Sam Branka <sdbranka@me.com>
2022-12-21 17:39:21 -0500

    added a comment

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game V1")
+# turn off screen animations so that we can use the update() method
 screen.tracer(0)
 
 snake = Snake()
@@ -24,7 +25,8 @@ game_is_on = True
 while game_is_on:
     # updates the screen after each move
         # placed outside of the move loop
-        # prevents seeing each independent segment move individually
+        # prevents seeing each independent segment 
+        # move individually
     screen.update()
     # delays the time cycle of each move by 0.1 second
     time.sleep(0.1)
